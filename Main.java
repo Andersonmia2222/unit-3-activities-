85577182d05a558a0ce8e576d591c332e9224055
Andersonmia2222 <mia.anderson2026@gbwl.org>
2026-01-13 16:03:29 +0000

    updated

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      System.out.println("Hello");
+
+       // CREATE 2 Pet objects with different initial values
+    
+         // TEST all Pet methods (getters, toString, setters)
+
+    
    }
 }
