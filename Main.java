f3dd8210127dc91895f8c126a34acc8e7b970f6f
Andersonmia2222 <mia.anderson2026@gbwl.org>
2026-01-14 17:17:51 +0000

    updated

--- Main.java
+++ Main.java
@@ -1,12 +1,21 @@
 public class Main {
+public static void main(String []args) {
+   Pet basicPet = new Pet("Buddy", "dog", 3, 25.5, true);
+   System.out.println(basicPet);
+   Pet pet2 = new Pet("Mochi", "cat", 1, 8.2, false);
 
-   public static void main(String []args) {
-      System.out.println("Hello");
+   System.out.println(pet2);
+   System.out.println( pet2.getName() );
+   System.out.println( pet2.getType() );
 
-       // CREATE 2 Pet objects with different initial values
-    
-         // TEST all Pet methods (getters, toString, setters)
+   int age = pet2.getAge();
+   System.out.println(age);
 
-    
-   }
+   pet2.setName("Mochi");
+   pet2.setAge(2); 
+   pet2.setWeight(9.0);
+   System.out.println(pet2);
+
+}
+
 }
